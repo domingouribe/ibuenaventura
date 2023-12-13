37be2da9cb9f94ea8bb2bb6deaad21751ffe6a81
Domingo Uribe <uribe.domingo@gmail.com>
2023-12-13 11:06:17 -0300

    applied data

---
+++ src/data/database.ts
@@ -0,0 +1,40 @@
+// Main menu elements
+const menuItems = [
+  { label: "Somos", link: "/somos" },
+  { label: "Proyectos", link: "/proyectos" },
+  { label: "Recibimos tu propiedad", link: "/recibimos-tu-propiedad" },
+  { label: "Postventa", link: "/postventa" },
+];
+
+//Hero bar items
+const heroBar = [
+  {
+    icon: "assets/icons/dollar.svg",
+    title: "Paga tu pie en cuotas",
+    text: "con tarjeta de crédito sin intereses",
+  },
+  {
+    icon: "assets/icons/percent-svgrepo-com.svg",
+    title: "Descuentos exclusivos",
+    text: "en unidades seleccionadas",
+  },
+  {
+    icon: "assets/icons/home-svgrepo-com.svg",
+    title: "Recibimos tu propiedad",
+    text: "como parte de pago",
+  },
+  {
+    icon: "assets/icons/360-degrees-svgrepo-com.svg",
+    title: "Recorre",
+    text: "piloto 360°",
+  },
+];
+
+
+
+
+
+
+
+
+export { menuItems, heroBar };

503c44e0c535963e828b02ea1304cb6959e093d6
Domingo Uribe <uribe.domingo@gmail.com>
2023-12-13 21:28:38 -0300

    Slider ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ibuenaventura.github.io"
-  //base: '/ibuenaventura'
-  ,
+
   integrations: [react()]
 });
